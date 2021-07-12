b4f500b1c8300e67ab54ba8ef003508233164b87
Jan Kotrc <john.kotrc@gmail.com>
2021-07-12 20:19:44 +0200

    sample sound now being made

--- soundmaker.py
+++ soundmaker.py
@@ -7,16 +7,37 @@ from tonesplitter import ToneSplitter
 samplerate=44100
 
 #TODO type hints for float ndarray
-def make_sound(data, scalefactor):
+def make_sound(data, scalefactor=32767):
     if data.dtype != 'float64':
         raise ValueError(f"{type(data)} not a float array!")
-    scaled = np.int16(data/np.max(np.abs(data)) * 32767)
-    write('test.wav', samplerate,scaled)
+    scaled = np.int16(data/np.max(np.abs(data)) * scalefactor)
+    write('test.wav', samplerate,scaled) #TODO check if filename exists
 
 '''
-1000hz, 1000 times per second with 10*10^-4 = 1/1000 s breaks
+1000hz, 1000 times per second.
+Let T denote the period of each signal segment and B the length of the break.
+Then 1000*(T+B)=1, T+B = 1/1000, where T,B < 1/1000 since they both have to be positive.
+
+choose 1/1100 long breaks.
+T+1/1100=1/1000
+1/1000-1/1100 = T
 '''
-def thousandthousand():
+
+#breaks of length 1/1100
+#normal sine wave
+def thousandthousand(duration): #duration in seconds
+    break_length = 1/1100
+    period = 1/1000-break_length
     t = Tone(1000, samplerate=samplerate)
     splitter = ToneSplitter(t)
-    splitter.setperiod()
+    splitter.setperiod(period)
+    silence = np.zeros(int(np.ceil(break_length*samplerate)))
+    sound = splitter.Next()
+    sound = np.append(sound,silence)
+    for i in range(0, duration*1000-1):
+        sound = np.append(sound,splitter.Next())
+        sound = np.append(sound,silence)
+    make_sound(sound)
+
+if __name__ == "__main__":
+    thousandthousand(3)
